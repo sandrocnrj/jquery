e8f5dfda62661f94c6f02e72f99024e88f474719
Sandro Carvalho <sandro.prog@gmail.com>
2016-11-24 23:25:07 -0200

    Alteração na main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,14 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
-console.log(numPalavras);
+var campo = $(".campo-digitacao");
+campo.on("click", function() {
+	var conteudo = campo.val();
+
+	var qtdPalavras = conteudo.split(" ").length;
+	$("#contador-palavras").text(qtdPalavras);
+
+	var qtdCaracteres = conteudo.length;
+	$("#contador-caracteres").text(qtdCaracteres);
+});
+
