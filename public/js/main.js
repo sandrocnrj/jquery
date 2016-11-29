1df68b1f1332681e32c7b84260b722b81f59ce7c
Sandro Carvalho <sandro.prog@gmail.com>
2016-11-29 13:53:05 -0200

    Melhorando os estilos

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,25 @@ $(function(){
 	atualizaTamanhoFrase();
 	inicializaContadores();
 	inicializaCronometro();
+	inicializarMarcadores();
 	$("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function inicializarMarcadores() {
+	var frase = $(".frase").text();
+	campo.on("input", function() {
+		var digitado = campo.val();
+		var comparavel = frase.substr(0, digitado.length);
+		
+		if(digitado == comparavel) {
+			campo.removeClass("borda-vermelha");
+			campo.addClass("borda-verde");		
+		} else {		
+			campo.removeClass("borda-verde");	
+			campo.addClass("borda-vermelha");
+		}
+	});
+}
 
 function atualizaTamanhoFrase(){
 	var frase = $(".frase").text();
@@ -44,12 +61,16 @@ function inicializaCronometro(){
 }
 
 function reiniciaJogo(){
-	campo.attr("disabled", false);
 	campo.val("");
-	campo.toggleClass("campo-desativado");
+	campo.attr("disabled", false);
 	$("#contador-palavras").text("0");
 	$("#contador-caracteres").text("0");
 	$("#tempo-digitacao").text(tempoInicial);				
 
 	inicializaCronometro();
+
+	campo.toggleClass("campo-desativado");
+
+	campo.removeClass("borda-vermelha");
+	campo.removeClass("borda-verde");
 };
